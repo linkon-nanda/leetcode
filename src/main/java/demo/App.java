83ce753ab5b2f88b93fb05d95d8ccf8466dd0d48
Linkon Nanda <linkon.nanda11@gmail.com>
2025-04-03 23:10:42 +0530

    ME_QA_XLEETCODE1_MODULE_L1

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -13,10 +13,11 @@ public class App {
         //TODO: call your test case functions one after other here
 
         tests.testCase01();
+        tests.testCase02();
+        tests.testCase03();
+        tests.testCase04();
 
         //END Tests
-
-
         tests.endTest(); // End your test by clearning connections and closing browser
     }
 
--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -1,20 +1,24 @@
 package demo;
 
-import java.util.concurrent.TimeUnit;
-
+import java.time.Duration;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.logging.Level;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
 public class TestCases {
     ChromeDriver driver;
-    public TestCases()
-    {
+    WebDriverWait wait;
+
+    public TestCases() {
         System.out.println("Constructor: TestCases");
 
         WebDriverManager.chromedriver().timeout(30).setup();
@@ -30,27 +34,111 @@ public class TestCases {
         System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "chromedriver.log");
 
         driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         // Set browser to maximize and wait
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
     }
 
-    public void endTest()
-    {
+    public void endTest() {
         System.out.println("End Test: TestCases");
         driver.close();
         driver.quit();
 
     }
 
-    
-    public  void testCase01(){
+    public void testCase01() throws InterruptedException {
+        // Pseudocode
+        // 1.
         System.out.println("Start Test case: testCase01");
-        driver.get("https://www.google.com");
+        driver.get("https://leetcode.com");
+        Thread.sleep(2000);
+        String homeUrl = driver.getCurrentUrl();
+        if (homeUrl.contains("leetcode")) {
+
+            System.out.println("The URL of the Leetcode homepage contains leetcode : PASS");
+        } else {
+
+            System.out.println("The URL of the Leetcode homepage missing leetcode : FAIL");
+        }
+
+        System.out.println("end Test case: testCase01");
+    }
+
+    public void testCase02() throws InterruptedException {
+
+        System.out.println("Start Test case: testCase02");
+        WebElement viewQues = driver.findElement(By.xpath("(//p[@class='link'])[2]"));
+        viewQues.click();
+        Thread.sleep(3000);
+        String problemUrl = driver.getCurrentUrl();
+        if (problemUrl.contains("problemset")) {
+
+            System.out.println("The URL of the Leetcode questions page contains problemset : PASS");
+        } else {
+
+            System.out.println("The URL of the Leetcode questions page missing problemset : FAIL");
+        }
+        List<WebElement> questions = driver.findElements(By.xpath(
+                "//div[@role='cell' and contains(@class, 'flex items-center') and @style[contains(., 'width: 260px')]]"));
+        for (int i = 1; i < 6; i++) {
+
+            String text = questions.get(i).getText();
+            System.out.println("Questions " + text);
+            if (i == 1) {
+
+                if (text.contains("Two Sum")) {
+
+                    System.out.println("First Question is Two Sum : PASS");
+                } else {
+
+                    System.out.println("First Question is not Two Sum : FAIL");
+                }
+            }
+
+        }
+
         System.out.println("end Test case: testCase02");
     }
 
+    public void testCase03() {
 
+        System.out.println("Start Test case: testCase03");
+        WebElement twoSumQues = driver.findElement(By.xpath("//a[normalize-space()='1. Two Sum']"));
+        twoSumQues.click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='submissions_tab']")));
+        String twoSumUrl = driver.getCurrentUrl();
+        if (twoSumUrl.contains("two-sum")) {
+
+            System.out.println("The URL of the problem contains two-sum : PASS");
+        } else {
+
+            System.out.println("The URL of the problem missing two-sum : PASS : FAIL");
+        }
+
+        System.out.println("end Test case: testCase03");
+    }
+
+    public void testCase04() throws InterruptedException {
+
+        System.out.println("Start Test case: testCase04");
+        WebElement submissionsTab = driver.findElement(By.xpath("//div[@id='submissions_tab']"));
+        submissionsTab.click();
+        Thread.sleep(1000);
+        WebElement SignIn = driver.findElement(By.xpath("//a[normalize-space()='Register or Sign In']"));
+        String signinText = SignIn.getText();
+        if (signinText.equalsIgnoreCase("Register or Sign In")) {
+
+            System.out.println(
+                    "The message Register or Sign In is displayed when you click on the submissions tab : PASS");
+        } else {
+
+            System.out.println(
+                    "The message Register or Sign In is not displayed when you click on the submissions tab : FAIL");
+        }
+
+        System.out.println("end Test case: testCase04");
+    }
 }
